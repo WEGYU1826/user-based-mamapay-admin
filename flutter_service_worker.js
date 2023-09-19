2e780f55e0a0c74eac33f4500ef464f6853dca8c
wegyu <79543679+WEGYU1826@users.noreply.github.com>
2023-09-19 11:51:52 +0300

    financial account suspend and activated done

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,7 +18,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "992818a093109c43519ae835de7adfd1
 "assets/assets/images/profile.png": "4ac39f98106e89ef321104bd0cc4044f",
 "assets/assets/images/undraw_page_not_found_re_e9o6.svg": "9db21ea7c8fe15417d7e74ba0ed5bbbb",
 "assets/FontManifest.json": "d4a4e9ead2373d032bf298d5b53e739f",
-"assets/fonts/MaterialIcons-Regular.otf": "d246ed8a2fc531df020adc3ae1eaec45",
+"assets/fonts/MaterialIcons-Regular.otf": "8501c6af0c26fdcef9e6a0bf8e95d91c",
 "assets/NOTICES": "1ba07ab9b6c235469936f8d43310fec9",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "57d849d738900cfd590e9adc7e208250",
 "assets/packages/flutter_dropzone_web/assets/flutter_dropzone.js": "31db837360ad5066e5c412067e806d54",
@@ -39,9 +39,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "992818a093109c43519ae835de7adfd1
 "icons/Icon-512.png": "eca475bbde94f49d4146bcf3e8dcfdb3",
 "icons/Icon-maskable-192.png": "d2b963f3010aa933e1b74b1e2f093e01",
 "icons/Icon-maskable-512.png": "eca475bbde94f49d4146bcf3e8dcfdb3",
-"index.html": "440f28d469c5be2f19990cf9d955b715",
-"/": "440f28d469c5be2f19990cf9d955b715",
-"main.dart.js": "3041b914f25ad8028d9d620debf3b61e",
+"index.html": "1a463583db7efe6f434c16b9abacb072",
+"/": "1a463583db7efe6f434c16b9abacb072",
+"main.dart.js": "19f394456db0fe70ce8642d4c41c0bd4",
 "manifest.json": "9c1d814d5c2515333b93005c4afadefa",
 "version.json": "6d571d350e3dd12f60940604cee4552b"};
 // The application shell files that are downloaded before a service worker can
