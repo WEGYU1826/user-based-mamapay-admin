2e11194d6d851b797b98a4e371cefd91dc552fb2
wegyu <awhm1826@gmail.com>
2025-01-10 15:06:54 +0300

    commit version 1.0.3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1442020699"
+    serviceWorkerVersion: "3633596104"
   }
 });
